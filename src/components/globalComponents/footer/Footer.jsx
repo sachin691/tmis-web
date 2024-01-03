5f66e13c583b4ead3f5fa704d47049a4433796c2
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-01-03 21:59:46 +0530

    Brand Logo Added

--- src/components/globalComponents/footer/Footer.jsx
+++ src/components/globalComponents/footer/Footer.jsx
@@ -1,20 +1,23 @@
 import { FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
-import { Divider } from "@nextui-org/react";
+import { Divider, Image } from "@nextui-org/react";
 import { Link } from "react-router-dom";
+
+// Local Files
+import logo from "../../../globalAssets/logo.svg";
 
 const Footer = () => {
   return (
     <div className="bg-slate-50 px-[3rem] md:px-[5rem] py-[5rem]">
       <div className="flex flex-col lg:flex-row gap-[4rem]">
-        <div>
-          <p className="font-bold text-xl">TMIS SOLUTIONS CO. LTD</p>
-          <p className="mt-[1rem]">
+        <div className="flex flex-col gap-[2rem]">
+          <Image width={300} alt="TMIS Image" src={logo} radius="none"/>
+          <p className="max-w-[25rem]">
             As a leading provider in the industry, TMIS is dedicated to serving international customers with utmost
             values.
           </p>
         </div>
-        <div className="hidden lg:block">
+        <div className="hidden lg:flex flex-col items-center justify-center">
           <Link to="../Services">
             <p className="p-[0.5rem] font-semibold text-[16px]">Services</p>
           </Link>
@@ -28,7 +31,7 @@ const Footer = () => {
             <p className="p-[0.5rem] font-semibold text-[16px]">Pricing</p>
           </Link>
         </div>
-        <div className="flex flex-col gap-[1rem]">
+        <div className="flex flex-col justify-center gap-[1rem]">
           <div className="flex flex-col md:flex-row gap-[1rem] md:gap-0 justify-evenly items-center">
             <div className="flex">
               <MdEmail className="w-[1.5rem] h-[1.5rem]" />
