907ba462011f2698eb495ae36d8e120e7f0ef01a
Sachin <sachinkaurav65@gmail.com>
2024-01-06 01:39:56 +0530

    automatic deployement check

--- src/components/contact/ContactUs.jsx
+++ src/components/contact/ContactUs.jsx
@@ -22,7 +22,7 @@ const ContactUs = () => {
           color="primary"
           radius="none"
         >
-          <span>Send Message</span>
+          <span>Send</span>
         </Button>
       </div>
       <div className="text-white max-w-[30rem] flex-col gap-[2rem] hidden lg:flex">
