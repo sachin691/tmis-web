b9766db5c6353de6c3af74338963071420de172d
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-01-07 20:04:53 +0530

    moving Emailjs cred to .env files

--- src/components/contact/ContactUs.jsx
+++ src/components/contact/ContactUs.jsx
@@ -6,14 +6,14 @@ import toast, { Toaster } from "react-hot-toast";
 import { scrollTop } from "../../utils/methods";
 import { useDispatch } from "react-redux";
 import { updateTab } from "../../store/curTabSlice";
+
 // Local Files
 import "./Contact.css";
 
 const emailRe = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
 const toastSetting = { position: "top-right" };
 
-const formNotFill = () =>
-  toast.error("Please Fill The Form Correctly", toastSetting);
+const formNotFill = () => toast.error("Please Fill The Form Correctly", toastSetting);
 const emailSent = () => toast.success("Email Sent", toastSetting);
 const emailNotSent = () => toast.error("Email Not Sent", toastSetting);
 
@@ -33,19 +33,13 @@ const ContactUs = () => {
   const [budgetState, setBudgetState] = useState(-1);
 
   const sendEmail = () => {
-    if (
-      userNameState > 0 &&
-      companyNameState > 0 &&
-      phoneState > 0 &&
-      budgetState > 0 &&
-      emailState > 0
-    ) {
+    if (userNameState > 0 && companyNameState > 0 && phoneState > 0 && budgetState > 0 && emailState > 0) {
       emailjs
         .sendForm(
-          "service_6p4ehwj",
-          "tmis_yr6qfud",
+          process.env.REACT_APP_SERVICE_ID,
+          process.env.REACT_APP_TEMPLET_ID,
           form.current,
-          "7YzRkbs9NFMM3jEW5"
+          process.env.REACT_APP_PUBLIC_KEY
         )
         .then(() => emailSent())
         .catch(() => emailNotSent());
@@ -70,9 +64,7 @@ const ContactUs = () => {
   return (
     <div className="ContactUs flex py-[6rem] justify-evenly items-center">
       <div className="flex flex-col ">
-        <h1 className="py-[1rem] font-['rubik_doodle_shadow']  text-white font-bold text-7xl">
-          Contact Us
-        </h1>
+        <h1 className="py-[1rem] font-['rubik_doodle_shadow']  text-white font-bold text-7xl">Contact Us</h1>
         <form
           className="w-[30rem] py-[2rem] flex-wrap md:flex-nowrap flex flex-col gap-[1rem]"
           ref={form}
@@ -91,9 +83,7 @@ const ContactUs = () => {
             label="Full Name"
             name="user_name"
             onChange={(event) => setUserNameState(event.target.value.length)}
-            errorMessage={
-              userNameState === 0 ? "Please enter a valid Name" : ""
-            }
+            errorMessage={userNameState === 0 ? "Please enter a valid Name" : ""}
             isInvalid={userNameState === 0}
           />
           <Input
@@ -101,9 +91,7 @@ const ContactUs = () => {
             label="Company Name"
             name="user_company_name"
             onChange={(event) => setCompanyNameState(event.target.value.length)}
-            errorMessage={
-              companyNameState === 0 ? "Please enter a valid Company Name" : ""
-            }
+            errorMessage={companyNameState === 0 ? "Please enter a valid Company Name" : ""}
             isInvalid={companyNameState === 0}
           />
           <Input
@@ -111,9 +99,7 @@ const ContactUs = () => {
             label="Phone Number"
             name="user_number"
             onChange={(event) => setPhoneState(event.target.value.length)}
-            errorMessage={
-              phoneState === 0 ? "Please enter a valid Phone Number" : ""
-            }
+            errorMessage={phoneState === 0 ? "Please enter a valid Phone Number" : ""}
             isInvalid={phoneState === 0}
           />
           <Input
@@ -129,9 +115,7 @@ const ContactUs = () => {
                   className="outline-none border-0 bg-transparent text-default-400 text-small"
                   id="currency"
                   name="user_currency"
-                  onChange={(event) =>
-                    (budgetType.current = event.target.value)
-                  }
+                  onChange={(event) => (budgetType.current = event.target.value)}
                 >
                   <option value="INR">INR</option>
                   <option value="USD">USD</option>
@@ -139,20 +123,11 @@ const ContactUs = () => {
               </div>
             }
             onChange={(event) => setBudgetState(event.target.value.length)}
-            errorMessage={
-              budgetState === 0 ? "Please enter a valid Budget" : ""
-            }
+            errorMessage={budgetState === 0 ? "Please enter a valid Budget" : ""}
             isInvalid={budgetState === 0}
           />
         </form>
-        <Button
-          size="lg"
-          className="w-[30rem] "
-          variant="bordered"
-          color="primary"
-          radius="none"
-          onClick={sendEmail}
-        >
+        <Button size="lg" className="w-[30rem] " variant="bordered" color="primary" radius="none" onClick={sendEmail}>
           <span>Send Message</span>
         </Button>
         <Toaster />
@@ -160,15 +135,12 @@ const ContactUs = () => {
       <div className="text-white max-w-[30rem] flex-col gap-[2rem] hidden lg:flex">
         <h1 className="font-bold text-[3rem] leading-[3rem]">TMIS Solutions</h1>
         <p>
-          We're eager to hear from you! Whether you have inquiries about our
-          international customer service solutions, are interested in exploring
-          partnership opportunities, or simply want to learn more about how TMIS
-          can elevate your business, fill out the form below. Our team is ready
-          to connect with you and provide tailored solutions to meet your unique
-          needs. Let's start a conversation and embark on a journey towards
-          unparalleled success together. Your satisfaction is our priority, and
-          we look forward to being your trusted partner in international
-          customer service excellence.
+          We're eager to hear from you! Whether you have inquiries about our international customer service solutions,
+          are interested in exploring partnership opportunities, or simply want to learn more about how TMIS can elevate
+          your business, fill out the form below. Our team is ready to connect with you and provide tailored solutions
+          to meet your unique needs. Let's start a conversation and embark on a journey towards unparalleled success
+          together. Your satisfaction is our priority, and we look forward to being your trusted partner in
+          international customer service excellence.
         </p>
       </div>
     </div>
