89e89e567f50428e8d872168fc1e97a4cfaa39d9
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-02-14 15:56:26 +0530

    updated refund and t&C

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Pricing from "./components/pricing/Pricing";
 import Footer from "./components/globalComponents/footer/Footer";
 import ContactUs from "./components/contact/ContactUs";
 import Privacy from "./components/privacy/Privacy";
+import Refund from "./components/globalSubComponents/Refund";
+import Terms from "./components/globalSubComponents/Terms";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
         <Route path="/About" element={<About />} />
         <Route path="/ContactUs" element={<ContactUs />} />
         <Route path="/Privacy&Policy" element={<Privacy />} />
+        <Route path="/Refund" element={<Refund />} />
+        <Route path="/T&C" element={<Terms />} />
         <Route path="*" element={<Navigate to="/Home" />} />
       </Routes>
       <Footer />
